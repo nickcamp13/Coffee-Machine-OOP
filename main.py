1060a00335656742dc12501cf1e90440cfaea935
Nick Campos <nickreyescampos@gmail.com>
2022-08-21 17:16:12 -1000

    coffee machine now functions

--- main.py
+++ main.py
@@ -2,3 +2,23 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+coffee_menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+while True:
+    drink = input("What would you like? (espresso/latte/cappuccino/frappe): ").lower()
+    if drink == "off":
+        break
+    elif drink == "report":
+        coffee_maker.report()
+    else:
+        order = coffee_menu.find_drink(drink)
+        if order not in coffee_menu.menu:
+            print("That is not something on the menu. Please enter your order again.")
+            continue
+
+        if coffee_maker.is_resource_sufficient(order):
+            if money_machine.make_payment(order.cost):
+                coffee_maker.make_coffee(order)
+                print(f"Here is your {drink}. Enjoy!")
--- menu.py
+++ menu.py
@@ -17,6 +17,7 @@ class Menu:
             MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5),
             MenuItem(name="espresso", water=50, milk=0, coffee=18, cost=1.5),
             MenuItem(name="cappuccino", water=250, milk=50, coffee=24, cost=3),
+            MenuItem(name="frappe", water=275, milk=200, coffee=30, cost=3.5),
         ]
 
     def get_items(self):
